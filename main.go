d6c75fa8e9a7e63efefe83092106786c16607d28
m19e <nexus.me7290@gmail.com>
2020-03-15 14:55:07 +0900

    DOMアクセスまで

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func print(this js.Value, i []js.Value) interface{} {
 	return nil
 }
 
+func manupilateDom(this js.Value, i []js.Value) interface{}  {
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", "WebAssembly!")
+	return nil
+}
+
 func registerCallbacks()  {
 	js.Global().Set("print", js.FuncOf(print))
+	js.Global().Set("manupilateDom", js.FuncOf(manupilateDom))
 }
 
 func main() {
