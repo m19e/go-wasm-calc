17a058ec120e0450c56cef19e22bc8a724fbc6bf
m19e <nexus.me7290@gmail.com>
2020-03-18 21:28:32 +0900

    完成
    あとでオペレータ分岐の関数化とかします

--- main.go
+++ main.go
@@ -1,23 +1,95 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
 	"syscall/js"
 )
 
-func print(this js.Value, i []js.Value) interface{} {
-	fmt.Println(i[0])
+var (
+	currentNum  = ""
+	accumulator = ""
+	operator = None
+)
+
+type Operator int
+
+const (
+	Plus Operator = iota
+	Sub
+	Mul
+	Div
+	None
+)
+
+func inputNum(this js.Value, i []js.Value) interface{} {
+	currentNum += i[0].String()
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", currentNum)
 	return nil
 }
 
-func manupilateDom(this js.Value, i []js.Value) interface{}  {
-	js.Global().Get("document").Call("getElementById", "result").Set("textContent", "WebAssembly!")
+func clearNum(this js.Value, i []js.Value) interface{} {
+	currentNum = ""
+	accumulator = ""
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", "0")
 	return nil
 }
 
-func registerCallbacks()  {
-	js.Global().Set("print", js.FuncOf(print))
-	js.Global().Set("manupilateDom", js.FuncOf(manupilateDom))
+func doPlus(this js.Value, i []js.Value) interface{} {
+	accumulator += currentNum
+	currentNum = ""
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", "0")
+	return nil
+}
+
+func doOperate(this js.Value, i []js.Value) interface{} {
+	accumulator += currentNum
+	currentNum = ""
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", "0")
+
+	switch i[0].String() {
+	case "+":
+		operator = Plus
+	case "-":
+		operator = Sub
+	case "*":
+		operator = Mul
+	case "/":
+		operator = Div
+	default:
+		operator = None
+	}
+
+	return nil
+}
+
+func doEqual(this js.Value, i []js.Value) interface{} {
+	int1, _ := strconv.Atoi(currentNum)
+	int2, _ := strconv.Atoi(accumulator)
+
+	accumulator = ""
+
+	var result int
+	switch operator {
+	case Plus:
+		result = int1 + int2
+	case Sub:
+		result = int2 - int1
+	case Mul:
+		result = int1 * int2
+	case Div:
+		result = int2 / int1
+	}
+
+	js.Global().Get("document").Call("getElementById", "result").Set("textContent", result)
+	currentNum = strconv.Itoa(result)
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("inputNum", js.FuncOf(inputNum))
+	js.Global().Set("clearNum", js.FuncOf(clearNum))
+	js.Global().Set("doOperate", js.FuncOf(doOperate))
+	js.Global().Set("doEqual", js.FuncOf(doEqual))
 }
 
 func main() {
